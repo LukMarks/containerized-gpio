1c83141a08a300f7bf88b1fe20c58ea345e2845f
Lucas Marques Moreno <lucas.mmoreno@hotmail.com>
2023-10-17 20:01:17 -0300

    Added inital files

---
+++ blink.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as gpio
+import time
+import logging
+import os
+
+logging.basicConfig(
+                    filename=os.getenv("LOG_FILE", "/home/lucas/projects/container-gpio/logs/local_blink.log"),
+                    format='%(asctime)s, %(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=os.getenv("LOGLEVEL", logging.INFO)
+                   )
+
+led_pin = os.getenv("LEDPIN", 8)
+
+
+logging.info("Start Board Setup")
+gpio.setwarnings(False)
+gpio.setmode(gpio.BOARD)
+logging.info("Finished Board Setup")
+
+def blink_led(pin:int, interval:int) -> None:
+
+	gpio.output(pin, gpio.HIGH)
+	logging.info("Set Pin " + str(pin) + " to HIGH")
+	time.sleep(interval)
+	gpio.output(pin, gpio.LOW)
+	logging.info("Set Pin " + str(pin) + " to LOW")
+	time.sleep(interval)
+
+try:
+
+	gpio.cleanup()
+	gpio.setup(led_pin, gpio.OUT, initial=gpio.LOW)
+
+	while True:
+		blink_interval = os.getenv("INTERVAL", 1)
+		blink_led(pin=led_pin, interval=blink_interval)
+except KeyboardInterrupt:
+	logging.info("Closing blink_pin.py routine")
+	gpio.cleanup()
+	logging.info("Cleared Board Configurations")
---
+++ dockerize.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker build --pull -t blink:latest -f docker/Dockerfile . 
+docker run -e LOG_FILE="/home/logs/blink_led.log" \
+           -e LOGLEVEL="logging.INFO" \
+           -e LEDPIN=8 \
+           -e INTERVAL=1\
+           --device /dev/gpiomem \ 
+           -v /home/lucas/projects/container-gpio/logs:/home/logs \ 
+            blink
