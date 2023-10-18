b1c4bb06245217b275e5507f4b0462bc2876d0ff
Lucas Marques Moreno <lucas.mmoreno@hotmail.com>
2023-10-18 18:53:07 -0300

    Fixed device permititions

--- dockerize.sh
+++ dockerize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker build --pull -t blink:latest -f docker/Dockerfile . 
 docker run -e LOG_FILE="/home/logs/blink_led.log" \
-           -e LOGLEVEL="logging.INFO" \
+           -e LOGLEVEL="info" \
            -e LEDPIN=8 \
-           -e INTERVAL=1\
-           -v /sys:/sys \ 
-           -v /home/lucas/projects/container-gpio/logs:/home/logs \ 
-            blink
+           -e INTERVAL=1 \
+           -v /home/lucas/projects/container-gpio/logs:/home/logs \
+           --device /dev/gpiomem \
+           blink
